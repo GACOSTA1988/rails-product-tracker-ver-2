e40f2c5fe9635186302c501fed60d971725a294e
George Acosta <georgeacostar@gmail.com>
2020-01-17 15:44:46 -0800

    add eating validations and pass test specs

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,4 +5,7 @@ class Review < ApplicationRecord
     validates :rating, presence: true
     validates :content_body, length: { minimum: 50 }
     validates :content_body, length: { maximum: 250 }
+    validates :rating, numericality: { only_integer: true }
+    validates :rating, numericality: { greater_than_or_equal_to: 1 }
+    validates :rating, numericality: { less_than_or_equal_to: 5 }
   end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -7,4 +7,7 @@ describe Review do
   it { should validate_presence_of :rating }
   it { should validate_length_of(:content_body).is_at_most(250) }
   it { should validate_length_of(:content_body).is_at_least(50) }
+  it { should validate_numericality_of(:rating).only_integer }
+  it { should validate_numericality_of(:rating).is_less_than_or_equal_to(5) }
+  it { should validate_numericality_of(:rating).is_greater_than_or_equal_to(1) }
 end
