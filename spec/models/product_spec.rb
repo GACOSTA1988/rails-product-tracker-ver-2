d78beb62ff84ecd5856aac1a54189754d0b94b61
George Acosta <georgeacostar@gmail.com>
2020-01-17 16:02:01 -0800

    add callback to product name

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -6,3 +6,9 @@ describe Product do
     it { should validate_presence_of :cost }
     it { should validate_presence_of :country_of_origin }
   end
+  describe Product do
+    it("titleizes the name of an product") do
+      product = Product.create({name: "cheese wiz", cost: "2", country_of_origin:"US"  })
+      expect(product.name()).to(eq("Cheese Wiz"))
+    end
+  end
